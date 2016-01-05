5fc3632903b7464f925207dacd9eac74307871c3
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-01-05 14:08:31 +0000

    Port demoninator kernel to CUDA

--- src/kernels/calc_denominator.cu
+++ src/kernels/calc_denominator.cu
@@ -1,26 +1,24 @@
 
-#pragma OPENCL EXTENSION cl_khr_fp64 : enable
-
 #define DENOMINATOR_INDEX(a,g,i,j,k,nang,ng,nx,ny) ((a)+((nang)*(g))+((nang)*(ng)*(i))+((nang)*(ng)*(nx)*(j))+((nang)*(ng)*(nx)*(ny)*(k)))
 #define denominator(a,g,i,j,k) denominator[DENOMINATOR_INDEX((a),(g),(i),(j),(k),nang,ng,nx,ny)]
 
-kernel void calc_denominator(
+__global__ void calc_denominator(
     const unsigned int nx,
     const unsigned int ny,
     const unsigned int nz,
     const unsigned int nang,
     const unsigned int ng,
-    global const double * restrict mat_cross_section,
-    global const double * restrict velocity_delta,
-    global const double * restrict mu,
-    global const double * restrict dd_i,
-    global const double * restrict dd_j,
-    global const double * restrict dd_k,
-    global double * restrict denominator
+    const double * restrict mat_cross_section,
+    const double * restrict velocity_delta,
+    const double * restrict mu,
+    const double * restrict dd_i,
+    const double * restrict dd_j,
+    const double * restrict dd_k,
+    double * restrict denominator
     )
 {
-    size_t a = get_global_id(0);
-    size_t g = get_global_id(1);
+    size_t a = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t g = blockIdx.y * blockDim.y + threadIdx.y;
 
     for (unsigned int k = 0; k < nz; k++)
         for (unsigned int j = 0; j < ny; j++)
