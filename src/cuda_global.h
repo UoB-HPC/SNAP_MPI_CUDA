6cad54082df6c69edfcb4d1d53277085ee56ca57
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-01-11 13:56:02 +0000

    Again function def of cuda error check should be in extern C

--- src/cuda_global.h
+++ src/cuda_global.h
@@ -14,10 +14,19 @@
 * \brief Basic CUDA routines and global structures
 */
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 /**
 @{
 \brief Check for CUDA errors */
 void check_cuda_error(const char *msg, const int line, const char * file);
 #define check_cuda(msg) check_cuda_error((msg),__LINE__,__FILE__)
 /** @}*/
+
+#ifdef __cplusplus
+}
+#endif
 
